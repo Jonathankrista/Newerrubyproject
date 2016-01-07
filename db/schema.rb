c28584ae41b4f87f8ec71ddd6757752174d43c7f
Jon <jonathankrista@gmail.com>
2016-01-06 21:27:00 -0500

    Styled up forgot password page and a few other things

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,6 @@ ActiveRecord::Schema.define(version: 20160106013827) do
     t.string   "subcategory"
     t.datetime "created_at",                            null: false
     t.datetime "updated_at",                            null: false
-    t.string   "image"
     t.integer  "cached_votes_total",      default: 0
     t.integer  "cached_votes_score",      default: 0
     t.integer  "cached_votes_up",         default: 0
