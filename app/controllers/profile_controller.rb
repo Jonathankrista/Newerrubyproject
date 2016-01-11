1a6d91745fe33f254ce6b8cb4fdfbf0455c42a27
carlosprovisorio <carlosprovisorio@icloud.com>
2016-01-10 21:01:39 -0500

    Redefine font-size in the title portfolio

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -3,6 +3,8 @@ class ProfileController < ApplicationController
   def create_image
     current_user.update(profile_params)
     redirect_to page_profile_path
+  rescue ActionController::ParameterMissing
+    redirect_to page_profile_path, notice: "Please select an image!"
   end
 
 
