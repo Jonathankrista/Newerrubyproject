2f488efff4c9248d879c993f887c3f9c5bf48f14
carlosprovisorio <carlosprovisorio@icloud.com>
2016-01-13 19:54:45 -0500

    fixed bug config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -23,6 +23,5 @@ if Rails.env.production?
     config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
 
     config.fog_directory    = ENV['S3_BUCKET_NAME']
-    config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
   end
 end
